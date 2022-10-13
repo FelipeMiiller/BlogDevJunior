c79e3bbb2824789f2454e283050521f983f55570
Felipe Miiller <felipemiillerr@gmail.com>
2022-10-12 23:28:00 -0400

    Update apollo.ts

--- src/services/apollo.ts
+++ src/services/apollo.ts
@@ -7,6 +7,6 @@ import { ApolloClient,InMemoryCache,ApolloProvider,gql,} from "@apollo/client";
 
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.hygraph.com/v2/cl6w0h2cx3s0l01uq26vh0tjr/master",
+  uri: "link",
   cache: new InMemoryCache(),
 });
